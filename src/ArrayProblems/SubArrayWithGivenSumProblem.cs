a509a0a7a6a6423bebd283ecbea0de1b243bbaa4
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-05-10 18:40:13 +0530

    Added new problem : Count all distinct pairs with difference equal to k. (#2)
    * Added Kadens' algorithm
    
    * Added new problem : Count all distinct pairs with difference equal to k

---
+++ src/ArrayProblems/SubArrayWithGivenSumProblem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems
+{
+    /// <summary>
+    /// Find subarray with given sum (Nonnegative Numbers)
+    /// https://www.geeksforgeeks.org/find-subarray-with-given-sum/
+    /// </summary>
+    public class SubArrayWithGivenSumProblem
+    {
+        public (int startIndex, int length) Find(int[] values, int sum)
+        {
+            var startIndex = 0;
+            var length = 0;
+            var currentSum = 0;
+            for (var i = 0; i < values.Length; i++)
+            {
+                currentSum += values[i];
+                if (currentSum == sum)
+                {
+                    length++;
+                    break;
+                }
+                if (currentSum < sum)
+                {
+                    length++;
+                }
+                else
+                {
+                    var tempSum = currentSum - values[startIndex];
+                    do
+                    {
+                        currentSum = tempSum;
+                        length--;
+                        startIndex++;
+                    } while (tempSum > sum);
+                }
+            }
+
+            return (startIndex, length);
+        }
+    }
+}
