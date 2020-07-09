7a43e3040d4657acd87646d187cff9a2a2cc4eea
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-07-09 22:46:05 +0530

    Added two array problems

---
+++ src/ArrayProblems/LongestIncreasingSubArray/LongestIncreasingSubArrayV1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.LongestIncreasingSubArray
+{
+    /// <summary>
+    /// https://www.geeksforgeeks.org/longest-increasing-subarray/
+    /// </summary>
+    public class LongestIncreasingSubArrayV1
+    {
+        public (int startIndex, int length) Find(int[] values)
+        {
+            var maxLength = 1;
+            var currentLength = 1;
+            var currentIndex = 0;
+            var startIndex = 0;
+            var previousValue = values[0];
+
+            for (var i = 1; i < values.Length; i++)
+            {
+                if (values[i] >= previousValue)
+                {
+                    currentLength++;
+                }
+                else
+                {
+                    currentIndex = i;
+                    currentLength = 1;
+                }
+
+                if (currentLength > maxLength)
+                {
+                    maxLength = currentLength;
+                    startIndex = currentIndex;
+                }
+            }
+
+            return (startIndex, maxLength);
+        }
+    }
+}
---
+++ test/ArrayProblems.Tests/LongestIncreasingSubArray/LongestIncreasingSubArrayV1.cs
@@ -0,0 +1,28 @@
+﻿using ArrayProblems.LongestIncreasingSubArray;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.Tests.LongestIncreasingSubArray
+{
+    [TestClass]
+    public class LongestIncreasingSubArrayV1Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfIntegers_When_Find_Then_ReturnsLongestIncreasingSubArray()
+        {
+            //Given
+            var values = new int[] { 5, 6, 3, 5, 7, 8, 9, 1, 2 };
+            var problem = new LongestIncreasingSubArrayV1();
+            var expectedResult = (startIndex : 2, length : 5);
+
+            //When
+            var actualResult = problem.Find(values);
+
+            //Then
+            Assert.AreEqual(expectedResult.startIndex, actualResult.startIndex);
+            Assert.AreEqual(expectedResult.length, actualResult.length);
+        }
+    }
+}
