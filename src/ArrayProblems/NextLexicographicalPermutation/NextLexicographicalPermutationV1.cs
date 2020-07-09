0a8da37aeca72936b9ec097b488d463264b7ea94
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-07-09 22:52:41 +0530

    Added two array problems (#4)
    * Added Kadens' algorithm
    
    * Added new problem : Count all distinct pairs with difference equal to k
    
    * Added finding duplicates in array problem
    
    1. Added running median problem
    2. Added finding duplicates in array problem
    
    * Added two array problems
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ src/ArrayProblems/NextLexicographicalPermutation/NextLexicographicalPermutationV1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.NextLexicographicalPermutation
+{
+    /// <summary>
+    /// https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
+    /// </summary>
+    public class NextLexicographicalPermutationV1
+    {
+        public (bool hasNextPermutation, int[] values) Generate(int[] values)
+        {
+            var longestDecreasingStartIndex = values.Length - 1;
+            while (longestDecreasingStartIndex > 0
+                && values[longestDecreasingStartIndex - 1] >= values[longestDecreasingStartIndex])
+            {
+                longestDecreasingStartIndex--;
+            }   
+
+            // If there is no decreasing sequence
+            if (longestDecreasingStartIndex <= 0)
+            {
+                return (false, values);
+            }
+
+            var pivotIndex = longestDecreasingStartIndex - 1;
+            // If array is long, then binary search is efficient
+            for (var i = values.Length - 1; i > pivotIndex; i--)
+            {
+                if (values[i] > values[pivotIndex])
+                {
+                    var temp = values[i];
+                    values[i] = values[pivotIndex];
+                    values[pivotIndex] = temp;
+                    break;
+                }
+            }
+
+            Array.Reverse(values, longestDecreasingStartIndex, values.Length - longestDecreasingStartIndex);
+
+            return (true, values);
+        }
+    }
+}
---
+++ test/ArrayProblems.Tests/NextLexicographicalPermutation/NextLexicographicalPermutationV1Tests.cs
@@ -0,0 +1,31 @@
+﻿using ArrayProblems.NextLexicographicalPermutation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.Tests.NextLexicographicalPermutation
+{
+    [TestClass]
+    public class NextLexicographicalPermutationV1Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfIntegers_When_Generate_Then_ReturnsNextPermutationInLexicographicalOrder()
+        {
+            //Given
+            var values = new int[] { 0, 1, 2, 5, 3, 3, 0 };
+            var problem = new NextLexicographicalPermutationV1();
+            var expectedValues = new int[] { 0, 1, 3, 0, 2, 3, 5 };
+
+            //When
+            var actualValues = problem.Generate(values);
+
+            //Then
+            Assert.AreEqual(true, actualValues.hasNextPermutation);
+            for (var i = 0; i < values.Length; i++)
+            {
+                Assert.AreEqual<int>(expectedValues[i], actualValues.values[i]);
+            }
+        }
+    }
+}
