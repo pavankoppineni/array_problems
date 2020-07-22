a5d16c14fdae6a61c83239fb7bb3742444e5efbd
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-07-22 23:40:32 +0530

    Added new problems (#6)
    * Added Kadens' algorithm
    
    * Added new problem : Count all distinct pairs with difference equal to k
    
    * Added finding duplicates in array problem
    
    1. Added running median problem
    2. Added finding duplicates in array problem
    
    * Added two array problems
    
    * Added multiple array problems
    
    * Added majority element problem
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ src/ArrayProblems/KthSmallestElementInUnsortedArray/KthSmallestElementInUnsortedArrayV1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ArrayProblems.KthSmallestElementInUnsortedArray
+{
+    /// <summary>
+    /// https://www.geeksforgeeks.org/kth-smallestlargest-element-unsorted-array/
+    /// </summary>
+    public class KthSmallestElementInUnsortedArrayV1
+    {
+        public int Find(int[] values, int k)
+        {
+            Array.Sort(values);
+            return values[k - 1];
+        }
+    }
+}
---
+++ test/ArrayProblems.Tests/KthSmallestElementInUnsortedArray/KthSmallestElementInUnsortedArrayV1Tests.cs
@@ -0,0 +1,28 @@
+﻿using ArrayProblems.KthSmallestElementInUnsortedArray;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.Tests.KthSmallestElementInUnsortedArray
+{
+    [TestClass]
+    public class KthSmallestElementInUnsortedArrayV1Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfIntegers_When_Find_Then_ShouldReturnKthSmallestElement()
+        {
+            //Given
+            var values = new int[] { 7, 10, 4, 3, 20, 15 };
+            var k = 3;
+            var problem = new KthSmallestElementInUnsortedArrayV1();
+            var expectedResult = 7;
+
+            //When
+            var actualResult = problem.Find(values, k);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
