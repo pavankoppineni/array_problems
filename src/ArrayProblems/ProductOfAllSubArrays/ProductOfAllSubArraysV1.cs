d036b0af7ce011aa547d6f45161b8dbec8a77b0c
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-07-28 17:47:04 +0530

    Added product of all sub arrays problem

---
+++ src/ArrayProblems/ProductOfAllSubArrays/ProductOfAllSubArraysV1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.ProductOfAllSubArrays
+{
+    /// <summary>
+    /// https://www.geeksforgeeks.org/product-of-all-subarrays-of-an-array/
+    /// </summary>
+    public class ProductOfAllSubArraysV1
+    {
+        public double GetProduct(int[] values)
+        {
+            double result = 1;
+            var count = values.Length;
+            for (var i = 0; i < values.Length; i++)
+            {
+                var pow = count * (i + 1);
+                result = result * Math.Pow(values[i], pow);
+                count--;
+            }
+            return result;
+        }
+    }
+}
