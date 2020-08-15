6ce2b2a94bff85b2655d521f216962bfebeb045a
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-08-15 14:09:25 +0530

    Added new problem : finding missing number in sorted array

---
+++ src/ArrayProblems/FindMissingNumberInSortedArrayOfLimitedRange/FindMissingNumberInSortedArrayofLimitedArrangeV1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.FindMissingNumberInSortedArrayOfLimitedRange
+{
+    /// <summary>
+    /// https://www.geeksforgeeks.org/find-missing-number-sorted-array-limited-range/
+    /// </summary>
+    public class FindMissingNumberInSortedArrayofLimitedArrangeV1
+    {
+        public int Find(int[] values)
+        {
+            return Find(values, 0, values.Length - 1);
+        }
+
+        private int Find(int[] values, int start, int end)
+        {
+            if (start == end)
+            {
+                return start + 1;
+            }
+
+            var mid = (start + end) / 2;
+
+            var value = values[mid];
+
+            // Value is equal to mid element
+            if (value == mid + 1)
+            {
+                return Find(values, mid + 1, end);
+            }
+
+            // Value is greater than element
+            if (value > mid + 1)
+            {
+                return Find(values, start, mid);
+            }
+
+            return Find(values, mid, end);
+        }
+    }
+}
---
+++ test/ArrayProblems.Tests/FindMissingNumberInSortedArrayofLimitedArrange/FindMissingNumberInSortedArrayofLimitedArrangeV1Tests.cs
@@ -0,0 +1,43 @@
+﻿using ArrayProblems.FindMissingNumberInSortedArrayOfLimitedRange;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.Tests.FindMissingNumberInSortedArrayofLimitedArrange
+{
+    [TestClass]
+    public class FindMissingNumberInSortedArrayofLimitedArrangeV1Tests
+    {
+        private static IEnumerable<object[]> GetDataset()
+        {
+            var dataset = new List<object[]>();
+            dataset.Add(new object[]
+            {
+                new int[]{1, 3, 4, 5, 6},
+                2
+            });
+            dataset.Add(new object[]
+            {
+                new int[]{1, 2, 3, 4, 5, 7, 8, 9, 10},
+                6
+            });
+
+            return dataset;
+        }
+
+        [DynamicData(nameof(GetDataset), DynamicDataSourceType.Method)]
+        [DataTestMethod]
+        public void Given_ArrayOfIntegers_When_Find_Then_ShouldReturnMissingNumber(int[] values, int expectedMissingNumber)
+        {
+            //Given
+            var problem = new FindMissingNumberInSortedArrayofLimitedArrangeV1();
+
+            //When
+            var actualMissingNumber = problem.Find(values);
+
+            //Then
+            Assert.AreEqual(expectedMissingNumber, actualMissingNumber);
+        }
+    }
+}
