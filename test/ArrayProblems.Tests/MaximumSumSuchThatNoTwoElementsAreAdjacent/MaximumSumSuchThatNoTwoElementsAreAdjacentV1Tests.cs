59ac5cb64e01353a55b7ac7322f58d6fcb7bb8ea
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-08-18 15:49:35 +0530

    Added new problem (#11)
    * Added Kadens' algorithm
    
    * Added new problem : Count all distinct pairs with difference equal to k
    
    * Added finding duplicates in array problem
    
    1. Added running median problem
    2. Added finding duplicates in array problem
    
    * Added two array problems
    
    * Added multiple array problems
    
    * Added majority element problem
    
    * Added product of all sub arrays problem
    
    * Added new problems
    
    * Added new problem : finding missing number in sorted array
    
    * Added problem : Maximum sub such that no two elements are nnext to each other
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ test/ArrayProblems.Tests/MaximumSumSuchThatNoTwoElementsAreAdjacent/MaximumSumSuchThatNoTwoElementsAreAdjacentV1Tests.cs
@@ -0,0 +1,47 @@
+﻿using ArrayProblems.MaximumSumSuchThatNoTwoElementsAreAdjacent;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.Tests.MaximumSumSuchThatNoTwoElementsAreAdjacent
+{
+    [TestClass]
+    public class MaximumSumSuchThatNoTwoElementsAreAdjacentV1Tests
+    {
+        private static IEnumerable<object[]> GetDataset()
+        {
+            var dataset = new List<object[]>();
+            dataset.Add(new object[]
+            {
+                new int[] { 3, 1, 2, 100 },
+                103
+            });
+            dataset.Add(new object[]
+            {
+                new int[] { 5, 5, 10, 100, 10, 5 },
+                110
+            });
+            dataset.Add(new object[]
+            {
+                new int[] { 5, 5, 10, 40, 50, 35 },
+                80
+            });
+            return dataset;
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetDataset), DynamicDataSourceType.Method)]
+        public void Given_ArrayOfIntegers_When_FindSum_Then_ShouldReturnMaximumSum(int[] values, int expectedResult)
+        {
+            //Given
+            var problem = new MaximumSumSuchThatNoTwoElementsAreAdjacentV1();
+
+            //When
+            var actualResult = problem.GetMaximum(values);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
