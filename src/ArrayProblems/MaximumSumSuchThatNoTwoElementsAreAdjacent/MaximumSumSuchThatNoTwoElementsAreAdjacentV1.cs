28ea78bd3c076441c666cb55bd9db9384fdfecf4
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-08-18 15:42:33 +0530

    Added problem : Maximum sub such that no two elements are nnext to each other

---
+++ src/ArrayProblems/MaximumSumSuchThatNoTwoElementsAreAdjacent/MaximumSumSuchThatNoTwoElementsAreAdjacentV1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.MaximumSumSuchThatNoTwoElementsAreAdjacent
+{
+    /// <summary>
+    /// https://www.geeksforgeeks.org/maximum-sum-such-that-no-two-elements-are-adjacent/
+    /// </summary>
+    public class MaximumSumSuchThatNoTwoElementsAreAdjacentV1
+    {
+        public int GetMaximum(int[] values)
+        {
+            var inclusiveSum = values[0];
+            var exclusiveSum = 0;
+
+            for (var i = 1; i < values.Length; i++)
+            {
+                var currentMax = Math.Max(inclusiveSum, exclusiveSum);  
+
+                inclusiveSum = exclusiveSum + values[i];
+                exclusiveSum = currentMax;
+            }
+
+            return Math.Max(inclusiveSum, exclusiveSum);
+        }
+    }
+}
