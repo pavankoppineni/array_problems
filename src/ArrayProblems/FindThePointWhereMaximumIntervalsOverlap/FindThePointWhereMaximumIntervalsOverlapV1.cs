73af34de043266724a78c891f1bf95a7cd7e8d56
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-10-14 21:35:07 +0530

    Added new problems

---
+++ src/ArrayProblems/FindThePointWhereMaximumIntervalsOverlap/FindThePointWhereMaximumIntervalsOverlapV1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArrayProblems.FindThePointWhereMaximumIntervalsOverlap
+{
+    /// <summary>
+    /// https://www.geeksforgeeks.org/find-the-point-where-maximum-intervals-overlap/
+    /// </summary>
+    public class FindThePointWhereMaximumIntervalsOverlapV1
+    {
+        public int Find(int[] inTimeValues, int[] outTimeValues)
+        {
+            var min = inTimeValues.Min();
+            var max = outTimeValues.Max();
+
+            var intervals = new int[max];
+            foreach(var inTime in inTimeValues)
+            {
+                intervals[inTime - 1] += 1;
+            }
+
+            foreach(var outTime in outTimeValues)
+            {
+                intervals[outTime - 1] -= 1;
+            }
+
+            return MaximumInterval(intervals);
+        }
+
+        private int MaximumInterval(int[] intervals)
+        {
+            var maxPeople = 0;
+            var numberOfPeople = 0;
+            var maxInterval = 0;
+            for (var i = 0; i < intervals.Length; i++)
+            {
+                numberOfPeople += intervals[i];
+                if (numberOfPeople > maxPeople)
+                {
+                    maxInterval = i;
+                    maxPeople = numberOfPeople;
+                }
+            }
+
+            return maxInterval + 1;
+        }
+    }
+}
---
+++ test/ArrayProblems.Tests/FindThePointWhereMaximumIntervalsOverlap/FindThePointWhereMaximumIntervalsOverlapV1Tests.cs
@@ -0,0 +1,28 @@
+﻿using ArrayProblems.FindThePointWhereMaximumIntervalsOverlap;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.Tests.FindThePointWhereMaximumIntervalsOverlap
+{
+    [TestClass]
+    public class FindThePointWhereMaximumIntervalsOverlapV1Tests
+    {
+        [TestMethod]
+        public void Given_InTimeAndOutTimeIntervals_When_FindMaximumInterval_Then_ShouldReturnMaximumInterval()
+        {
+            //Given
+            var intTimeIntervals = new int[] { 1, 2, 9, 5, 5 };
+            var outTimeIntervals = new int[] { 4, 5, 12, 9, 12 };
+            var problem = new FindThePointWhereMaximumIntervalsOverlapV1();
+            var expectedResult = 3;
+
+            //When
+            var actualResult = problem.Find(intTimeIntervals, outTimeIntervals);
+
+            //Then
+            Assert.Inconclusive();
+        }
+    }
+}
