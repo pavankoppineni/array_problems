2fd0bf098f6a343dbb369f245ab5ea550412cd6b
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-10-14 21:36:50 +0530

    Added new problems (#13)
    * Added Kadens' algorithm
    
    * Added new problem : Count all distinct pairs with difference equal to k
    
    * Added finding duplicates in array problem
    
    1. Added running median problem
    2. Added finding duplicates in array problem
    
    * Added two array problems
    
    * Added multiple array problems
    
    * Added majority element problem
    
    * Added product of all sub arrays problem
    
    * Added new problems
    
    * Added new problem : finding missing number in sorted array
    
    * Added problem : Maximum sub such that no two elements are nnext to each other
    
    * Added new problems
    
    * Added new problems
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ src/ArrayProblems/SortArrayBasedOnAbsoluteValues/SortArrayBasedOnAbsoluteValuesV1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.SortArrayBasedOnAbsoluteValues
+{
+    public class SortArrayBasedOnAbsoluteValuesV1
+    {
+        public int[] Sort(int[] values)
+        {
+            var newValues = new int[values.Length];
+            var newIndex = values.Length - 1;
+            var left = 0;
+            var right = values.Length - 1;
+            while (left <= right)
+            {
+                var leftAbs = Math.Abs(values[left]);
+                var rightAbs = Math.Abs(values[right]);
+
+                // Case 1 : Left and right are equal
+                if (leftAbs == rightAbs)
+                {
+                    newValues[newIndex] = values[right];
+                    right -= 1;
+                }
+
+                // Case 2 : Left is less than right
+                else if (leftAbs < rightAbs)
+                {
+                    newValues[newIndex] = values[right];
+                    right -= 1;
+                }
+
+                // Case 3 : Left is greater than right
+                else
+                {
+                    newValues[newIndex] = values[left];
+                    left += 1;
+                }
+
+                newIndex -= 1;
+            }
+
+            return newValues;
+        }
+    }
+}
---
+++ test/ArrayProblems.Tests/SortArrayBasedOnAbsoluteValues/SortArrayBasedOnAbsoluteValuesV1Tests.cs
@@ -0,0 +1,30 @@
+﻿using ArrayProblems.SortArrayBasedOnAbsoluteValues;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.Tests.SortArrayBasedOnAbsoluteValues
+{
+    [TestClass]
+    public class SortArrayBasedOnAbsoluteValuesV1Tests
+    {
+        [TestMethod]
+        public void Given_SortedArray_When_Sort_Then_ShouldReturnArraySortedBasedOnAbsoluteValues()
+        {
+            // Given
+            var values = new int[] { -8, -5, -3, -1, 3, 6, 9 };
+            var expectedResult = new int[] { -1, -3, 3, -5, 6, -8, 9 };
+            var problem = new SortArrayBasedOnAbsoluteValuesV1();
+
+            // When
+            var actualResult = problem.Sort(values);
+
+            // Then
+            for(var i = 0; i < values.Length; i++)
+            {
+                Assert.AreEqual(expectedResult[i], actualResult[i]);
+            }
+        }
+    }
+}
