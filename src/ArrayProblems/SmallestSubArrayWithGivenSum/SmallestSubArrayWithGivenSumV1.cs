ec4d2fdf8bbc1a84e9e40a91e4291b93a70e2e48
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-10-18 18:36:40 +0530

    Added problem : smallest sub array with sum greater than given sum

---
+++ src/ArrayProblems/SmallestSubArrayWithGivenSum/SmallestSubArrayWithGivenSumV1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.SmallestSubArrayWithGivenSum
+{
+    /// <summary>
+    /// https://leetcode.com/problems/minimum-size-subarray-sum/
+    /// </summary>
+    public class SmallestSubArrayWithGivenSumV1
+    {
+        public int Find(int[] values, int sum)
+        {
+            var start = -1;
+            var currentSum = 0;
+            var length = 0;
+            var currentLength = 0;
+
+            for (var i = 0; i < values.Length; i++)
+            {
+                // Case 1 : If current value is greater than or equal to sum
+                if (values[i] >= sum)
+                {
+                    length = 1;
+                    break;
+                }
+
+                currentSum += values[i];
+                currentLength += 1;
+                if (start == -1)
+                {
+                    start = 0;
+                    currentLength = 1;
+                }
+                else
+                {
+                    if (currentSum >= sum)
+                    {
+                        while (start < i)
+                        {
+                            if ((currentSum - values[start]) >= sum)
+                            {
+                                currentSum -= values[start];
+                                start++;
+                                currentLength--;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        if (length == 0)
+                        {
+                            length = currentLength;
+                        }
+                        else if (currentLength < length)
+                        {
+                            length = currentLength;
+                        }
+                    }
+                }
+            }
+
+            return length;
+        }
+    }
+}
---
+++ test/ArrayProblems.Tests/SmallestSubArrayWithGivenSum/SmallestSubArrayWithGivenSumV1Tests.cs
@@ -0,0 +1,28 @@
+﻿using ArrayProblems.SmallestSubArrayWithGivenSum;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.Tests.SmallestSubArrayWithGivenSum
+{
+    [TestClass]
+    public class SmallestSubArrayWithGivenSumV1Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfPositiveIntegers_When_FindSmallestSubArray_Then_ShouldReturnLengthOfSmallestSubArrayWithGivenSum()
+        {
+            // Given
+            var values = new int[] { 2, 3, 1, 2, 4, 3 };
+            var sum = 7;
+            var problem = new SmallestSubArrayWithGivenSumV1();
+            var expectedResult = 3;
+
+            // When
+            var actualResult = problem.Find(values, sum);
+
+            // Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
