df9e650af4ea29929358c4ca52f3bf39cef8ac93
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-10-20 16:59:18 +0530

    Added new leet code problem (#17)
    * Added Kadens' algorithm
    
    * Added new problem : Count all distinct pairs with difference equal to k
    
    * Added finding duplicates in array problem
    
    1. Added running median problem
    2. Added finding duplicates in array problem
    
    * Added two array problems
    
    * Added multiple array problems
    
    * Added majority element problem
    
    * Added product of all sub arrays problem
    
    * Added new problems
    
    * Added new problem : finding missing number in sorted array
    
    * Added problem : Maximum sub such that no two elements are nnext to each other
    
    * Added new problems
    
    * Added new problems
    
    * Added problem : smallest sub array with sum greater than given sum
    
    * Fixed unit testing issue
    
    * Added new problems
    
    * Added leet code combination sum problem
    
    * Added new leet code probelms
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ src/ArrayProblems/Combination1SumLeetCode/Combination1SumLeetCodeV1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.Combination1SumLeetCode
+{
+    /// <summary>
+    /// https://leetcode.com/problems/combinations/
+    /// </summary>
+    public class Combination1SumLeetCodeV1
+    {
+            private int combination;
+            private int[] path;
+            private int max;
+            private IList<IList<int>> combinations;
+            public IList<IList<int>> FindCombinations(int number, int combination)
+            {
+                combinations = new List<IList<int>>();
+                this.max = number;
+                this.path = new int[combination];
+                this.combination = combination;
+                for (var i = 1; i <= number; i++)
+                {
+                    AddCombinations(i, 0);
+                }
+                return combinations;
+            }
+
+            private bool AddCombinations(int number, int level)
+            {
+                if(level == combination)
+                {
+                    var newCombination = new int[path.Length];
+                    Array.Copy(path, newCombination, path.Length);
+                    combinations.Add(newCombination);
+                    return false;
+                }
+                path[level] = number;
+                while (number <= max)
+                {
+                    number += 1;
+                    var result = AddCombinations(number, level + 1);
+                    if (!result)
+                    {
+                        break;
+                    }
+                }
+
+                return true;
+            }
+    }
+}
