73053aa3509ae75e1c02bddb45bd8708fe8dc38e
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-10-23 00:04:31 +0530

    Fixed unit testing issue

--- src/ArrayProblems.Shell/Program.cs
+++ src/ArrayProblems.Shell/Program.cs
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-    using System;
-=======
 using ArrayProblems.ProductOfAllSubArrays;
 using System;
->>>>>>> df9e650af4ea29929358c4ca52f3bf39cef8ac93
 using System.Collections.Generic;
 
 namespace ArrayProblems.Shell
