5fd0c84ab0da2a0e02b01c37a3eb29764e7fc72e
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-11-08 22:50:43 +0530

    Added new problem (#19)
    * Added Kadens' algorithm
    
    * Added new problem : Count all distinct pairs with difference equal to k
    
    * Added finding duplicates in array problem
    
    1. Added running median problem
    2. Added finding duplicates in array problem
    
    * Added two array problems
    
    * Added multiple array problems
    
    * Added majority element problem
    
    * Added product of all sub arrays problem
    
    * Added new problems
    
    * Added new problem : finding missing number in sorted array
    
    * Added problem : Maximum sub such that no two elements are nnext to each other
    
    * Added new problems
    
    * Added new problems
    
    * Added problem : smallest sub array with sum greater than given sum
    
    * Fixed unit testing issue
    
    * Added new problems
    
    * Added leet code combination sum problem
    
    * Added new leet code probelms
    
    * Added new problems
    
    * Fixed unit testing issue
    
    * Added new problem
    
    * Fixed unit testing issue
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ src/ArrayProblems/FindNumberOfPairsWithDifferenceLessThanOrEqualToK/FindNumberOfPairsWithDifferenceLessThanOrEqualToKV1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.FindNumberOfPairsWithDifferenceLessThanOrEqualToK
+{
+    /// <summary>
+    /// https://www.geeksforgeeks.org/pairs-difference-less-k/
+    /// </summary>
+    public class FindNumberOfPairsWithDifferenceLessThanOrEqualToKV1
+    {
+        public int FindNumberOfPairs(int[] values, int difference)
+        {
+            Array.Sort(values);
+            var result = 0;
+            var low = 0;
+            var high = 1;
+            while (low < values.Length && high < values.Length)
+            {
+                if (low == high)
+                {
+                    high += 1;
+                    continue;
+                }
+
+                // Case 1 : When difference between low and high is less than difference
+                // Action : Increate high
+                if (Math.Abs(values[low] - values[high]) <= difference)
+                {
+                    high += 1;
+                }
+                else
+                // Case 2 : When difference between low and high is greater than difference
+                // Action : 
+                {
+                    result += high - low - 1;
+                    low += 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ test/ArrayProblems.Tests/FindNumberOfPairsWithDifferenceLessThanOrEqualToK/FindNumberOfPairsWithDifferenceLessThanOrEqualToKV2Tests.cs
@@ -0,0 +1,28 @@
+﻿using ArrayProblems.FindNumberOfPairsWithDifferenceLessThanOrEqualToK;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.Tests.FindNumberOfPairsWithDifferenceLessThanOrEqualToK
+{
+    [TestClass]
+    public class FindNumberOfPairsWithDifferenceLessThanOrEqualToKV2Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfPositiveIntegersAndDifferene_When_FindNumberOfPairsWithDifference_Then_ShoulReturnCountOfPairsWithDifferenceLessThanGiveDifference()
+        {
+            //Given
+            var difference = 5;
+            var values = new int[] { 1, 2, 3, 4, 5, 6 };
+            var problem = new FindNumberOfPairsWithDifferenceLessThanOrEqualToKV2();
+            var expectedCount = 14;
+
+            //When
+            var actualCount = problem.CountAllPairs(values, difference);
+
+            //Then
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+    }
+}
