5791918b50c53f8768f7f70cae3e32613d1244d5
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-11-08 22:43:34 +0530

    Added new problem

---
+++ src/ArrayProblems/FindNumberOfPairsWithDifferenceLessThanOrEqualToK/FindNumberOfPairsWithDifferenceLessThanOrEqualToKV2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.FindNumberOfPairsWithDifferenceLessThanOrEqualToK
+{
+
+    /// <summary>
+    /// https://www.geeksforgeeks.org/pairs-difference-less-k/
+    /// </summary>
+    public class FindNumberOfPairsWithDifferenceLessThanOrEqualToKV2
+    {
+        private int[] values;
+        private int difference;
+
+        public int CountAllPairs(int[] values, int difference)
+        {
+            this.values = values;
+            this.difference = difference;
+            var endIndex = values.Length - 1;
+            var count = 0;
+            var startIndex = 0;
+            for (var i = 0; i < values.Length; i++)
+            {
+                if (i > startIndex)
+                {
+                    startIndex = i;
+                }
+                var currentHighIndex = FindHigIndex(i, startIndex, endIndex, -1);
+                count += currentHighIndex - i;
+                startIndex = currentHighIndex;
+            }
+            return count;
+        }
+
+        private int FindHigIndex(int currentIndex, int start, int end, int highIndex)
+        {
+            if(start > end)
+            {
+                return highIndex;
+            }
+
+            var mid = (start + end) >> 1;
+            var currentDifference = Math.Abs(values[currentIndex] - values[mid]);
+
+            //Case 1 : Current difference is less than difference
+            //Action : Search right half of an array
+            if(currentDifference < difference)
+            {
+                highIndex = mid;
+                return FindHigIndex(currentIndex, mid + 1, end, highIndex);
+            }
+
+            //Case 2 : Current difference is equal or greater than difference
+            //Action : Search left half of an array
+            else
+            {
+                return FindHigIndex(currentIndex, start, mid - 1, highIndex);
+            }
+        }
+    }
+}
---
+++ test/ArrayProblems.Tests/FindNumberOfPairsWithDifferenceLessThanOrEqualToK/FindNumberOfPairsWithDifferenceLessThanOrEqualToKV1Tests.cs
@@ -0,0 +1,28 @@
+﻿using ArrayProblems.FindNumberOfPairsWithDifferenceLessThanOrEqualToK;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayProblems.Tests.FindNumberOfPairsWithDifferenceLessThanOrEqualToK
+{
+    [TestClass]
+    public class FindNumberOfPairsWithDifferenceLessThanOrEqualToKV1Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfIntegersAndDifferenceK_When_CountPairs_Then_ShouldReturnNumberOfPairsLessThanOrEqualToDifferenceK()
+        {
+            //Given
+            var values = new int[] { 1, 10, 4, 2 };
+            var difference = 3;
+            var problem = new FindNumberOfPairsWithDifferenceLessThanOrEqualToKV1();
+            var expectedResult = -1;
+
+            //When
+            var actualResult = problem.FindNumberOfPairs(values, difference);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
